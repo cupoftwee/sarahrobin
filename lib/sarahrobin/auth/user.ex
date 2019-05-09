147898b27e8f60ec822c7336c12e64a60c34e556
Sarah Robin <burgundyshark@protonmail.com>
2019-05-09 10:22:05 -0700

    extend post and user modules to match new schema

--- lib/sarahrobin/auth/user.ex
+++ lib/sarahrobin/auth/user.ex
@@ -1,20 +1,27 @@
 defmodule Sarahrobin.Auth.User do
-  use Ecto.Schema
-  import Ecto.Changeset
+  # imports
+  # schema "users"
 
-  schema "users" do
-    field :email, :string
-    field :is_admin, :boolean, default: false
-    field :name, :string
-    field :password_hash, :string
+  @create_fields ~w(name password email)a
+  @optional_fields ~w(is_admin)a
 
-    timestamps()
-  end
-
-  @doc false
-  def changeset(user, attrs) do
+  def create_changeset(user, attrs) do
     user
-    |> cast(attrs, [:name, :email, :password_hash, :is_admin])
-    |> validate_required([:name, :email, :password_hash, :is_admin])
+    |> cast(attrs, @create_fields ++ @optional_fields)
+    |> validate_required(@create_fields)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:name, min: 3)
+    |> validate_length(:password, min: 3)
+    |> put_password_hash()
   end
+
+  defp put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
+  end
+
 end
--- lib/sarahrobin/content/post.ex
+++ lib/sarahrobin/content/post.ex
@@ -1,22 +1,34 @@
 defmodule Sarahrobin.Content.Post do
-  use Ecto.Schema
-  import Ecto.Changeset
+  # imports
 
-  schema "posts" do
-    field :body, :string
-    field :cover, :string
-    field :published, :boolean, default: false
-    field :slug, :string
-    field :title, :string
-    field :user_id, :integer
+  @derive {Phoenix.Param, key: :slug}
 
-    timestamps()
-  end
+  # schema "posts"
 
-  @doc false
-  def changeset(post, attrs) do
+  def create_changeset(post, attrs) do
     post
-    |> cast(attrs, [:title, :body, :published, :cover, :user_id, :slug])
-    |> validate_required([:title, :body, :published, :cover, :user_id, :slug])
+    |> common_changeset(attrs)
+    |> validate_required([:user_id, :cover])
   end
+
+  def common_changeset(changeset, attrs) do
+    changeset
+    |> cast(attrs, [:title, :body, :published, :user_id])
+    |> cast_attachments(attrs, [:cover])
+    |> validate_required([:title, :body])
+    |> validate_length(:title, min: 3)     
+    |> process_slug
+  end
+
+  # Private
+
+  defp process_slug(%Ecto.Changeset{valid?: validity, changes: %{title: title}} = changeset) do
+    case validity do
+      true -> put_change(changeset, :slug, Slugger.slugify_downcase(title))
+      false -> changeset
+    end
+  end
+
+  defp process_slug(changeset), do: changeset
+
 end

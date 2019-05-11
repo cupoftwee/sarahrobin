22bb13c40bc1e0ed03a82f77ccbaf749f4504060
Sarah Robin <burgundyshark@protonmail.com>
2019-05-10 17:29:39 -0700

    pls gigalixir work

--- config/prod.exs
+++ config/prod.exs
@@ -18,21 +18,32 @@ use Mix.Config
 
 # Gigalixir
 config :sarahrobin, SarahrobinWeb.Endpoint,
-  load_from_system_env: true,
-  http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
-  server: true, # Without this line, your app will not start the web server!
-  secret_key_base: "${SECRET_KEY_BASE}",
-  # secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  url: [host: "$sarahrobin.gigalixirapp.com", port: 443],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
 
 config :sarahrobin, Sarahrobin.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: "${DATABASE_URL}",
-  # url: System.get_env("DATABASE_URL"),
-  database: "",
+  url: System.get_env("DATABASE_URL"),
   ssl: true,
   pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections.
+# config :sarahrobin, SarahrobinWeb.Endpoint,
+#   load_from_system_env: true,
+#   http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
+#   server: true, # Without this line, your app will not start the web server!
+#   secret_key_base: "${SECRET_KEY_BASE}",
+#   # secret_key_base: System.get_env("SECRET_KEY_BASE"),
+#   url: [host: "$sarahrobin.gigalixirapp.com", port: 443],
+#   cache_static_manifest: "priv/static/cache_manifest.json"
+
+# config :sarahrobin, Sarahrobin.Repo,
+#   adapter: Ecto.Adapters.Postgres,
+#   url: "${DATABASE_URL}",
+#   # url: System.get_env("DATABASE_URL"),
+#   database: "",
+#   ssl: true,
+#   pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections.
 
 
 

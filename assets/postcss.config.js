8c42bb7f6ebd205400058601666bc5c6028ae0e8
Sarah Robin <burgundyshark@protonmail.com>
2019-05-10 20:15:56 -0700

    client - add and configure postcss

---
+++ assets/postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: {
+    "postcss-plugin": {},
+    "cssnano":{ preset: 'default' },
+    "postcss-import": {},
+    "postcss-normalize": {},
+    "autoprefixer": {},
+    "postcss-nested": {},
+    "postcss-simple-vars": {},
+    "postcss-easing-gradients": {},
+    "postcss-functions": {},
+    "cssnano": {}
+  }
+}

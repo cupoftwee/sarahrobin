58eb5a0d8bc7ece7eb48b3f6f62d7695353d944e
cupoftwee <sarahrobin@protonmail.com>
2021-08-12 11:35:07 -0700

    adds about page

---
+++ pages/about.vue
@@ -0,0 +1,50 @@
+<template>
+  <DefaultLayout title="About Me" mode="sinebow" glassBorderB>
+    <AuthorBio class="border-b-8 border-gradient" />
+
+    <!-- <section>
+      <post-item
+        v-for="edge in $page.author.belongsTo.edges"
+        :key="edge.node.id"
+        :post="edge.node"
+      />
+    </section>
+
+    <pagination
+      v-if="$page.author.belongsTo.pageInfo.totalPages > 1"
+      :base="`${$page.author.path}`"
+      :info="$page.author.belongsTo.pageInfo"
+    /> -->
+  </DefaultLayout>
+</template>
+
+<script>
+  import AuthorBio from '@/components/Biography'
+
+  export default {
+    components: {
+      AuthorBio
+    },
+
+    methods: {
+      titleCase(str) {
+        return str.replace('-', ' ')
+          .split(' ')
+          .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+          .join(' ')
+      }
+    }
+  }
+</script>
+
+<style>
+  .author-info {
+    @apply flex flex-col-reverse;
+    @apply mx-auto  max-w-xl px-6 pt-24 pb-10;
+    @apply text-center border-b-2 border-gray-300;
+  }
+
+  .author-name {
+    @apply text-4xl font-sans font-bold mb-2 capitalize text-white;
+  }
+</style>

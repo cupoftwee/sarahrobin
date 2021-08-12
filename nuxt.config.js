aab3674821fc1795fe0f5e5a52910cd6f580de8b
cupoftwee <sarahrobin@protonmail.com>
2021-08-12 11:21:13 -0700

    adds primary configs

---
+++ nuxt.config.js
@@ -0,0 +1,79 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Sarah Robin | Web Developer',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '~/assets/main.css'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    '~/plugins/fa-icons'
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+
+    ['@nuxtjs/google-fonts', {
+      'Crimson+Text':{ ital: [600]},
+      Oswald: [600],
+      'Fira+Sans': [200, 300, 400, 600, 700]
+    }]
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+  ],
+
+  hooks: {
+    'content:file:beforeInsert': (document) => {
+      if (document.extension === '.md') {
+        const timeStats = require('reading-time')(document.text)
+
+        document.readingTime = timeStats.text
+      }
+    }
+  },
+
+  // Content module configuration: https://go.nuxtjs.dev/config-content
+  content: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    babel: {
+      plugins: [['@babel/plugin-proposal-private-property-in-object', { loose: true }]],
+    },
+    postcss: {
+      plugins: {
+        "postcss-nested": {},
+        "postcss-import": {},
+        "postcss-simple-vars": {}
+      }
+    }
+  }
+}
